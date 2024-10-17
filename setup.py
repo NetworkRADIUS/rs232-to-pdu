e4410861d7692e2406f37a6e73bcc310ea6d3b16
Patrick Guo <patrick.pguo@gmail.com>
2024-10-17 18:43:20 +0100

    Updating version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rs232-to-tripplite-pdu',
-    version='1.0.2',
+    version='1.0.4',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 
