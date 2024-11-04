533237609b2cb31129dc550c7ac282ff558157dd
Patrick Guo <pguo@inkbridge.io>
2024-11-04 15:41:56 -0500

    Make configurable logging optional

--- src/rs232_to_tripplite/__main__.py
+++ src/rs232_to_tripplite/__main__.py
@@ -16,11 +16,15 @@ CONFIG_FILE = pathlib.Path('config.yaml')
 with open(CONFIG_FILE, 'r', encoding='utf-8') as fileopen:
     config = yaml.load(fileopen, Loader=yaml.FullLoader)
 
-if len(tuple(config['log'].keys())) > 1:
-    raise ValueError('More than one log file found')
+if 'log' in config:
+    if len(tuple(config['log'].keys())) > 1:
+        raise ValueError('More than one log file found')
 
-for i, key in enumerate(config['log']):
-    nrlogfac.setup_logging(key, config['log'][key])
+    for i, key in enumerate(config['log']):
+        nrlogfac.setup_logging(key, config['log'][key])
+else:
+    # default to stdout
+    nrlogfac.setup_logging('stream', 'stdout')
 
 if __name__ == '__main__':
     serial_listener = Rs2323ToTripplite(
