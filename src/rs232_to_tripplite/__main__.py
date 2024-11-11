a3dce80e02fb05e5a223b462d75a8860c53d0f63
Patrick Guo <pguo@inkbridge.io>
2024-11-11 14:28:27 -0500

    bug fix: adding in code lost from merge

--- src/rs232_to_tripplite/__main__.py
+++ src/rs232_to_tripplite/__main__.py
@@ -8,23 +8,12 @@ import pathlib
 
 import yaml
 
-import rs232_to_tripplite.logfactory as logfac
 from rs232_to_tripplite.rs232tripplite import Rs2323ToTripplite
 
 # Read and setup configs
 CONFIG_FILE = pathlib.Path('config.yaml')
 with open(CONFIG_FILE, 'r', encoding='utf-8') as fileopen:
     config = yaml.load(fileopen, Loader=yaml.FullLoader)
-
-if 'log' in config:
-    if len(tuple(config['log'].keys())) > 1:
-        raise ValueError('More than one log file found')
-
-    for i, key in enumerate(config['log']):
-        logfac.setup_logging(key, config['log'][key])
-else:
-    # default to stdout
-    logfac.setup_logging('stream', 'stdout')
 
 if __name__ == '__main__':
     serial_listener = Rs2323ToTripplite(
